06fa55962d488a6ab1fbb5e7925bde5e40b8936d
marc <m4rc.3iro@gmail.com>
2018-08-08 15:24:50 +0200

    First chunk of dynamic retrieval of data for profile content. Only ITRA Perfomance Index is fed back into the page.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Profile } from './profile';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  // performanceGeneral: string[];
+  performanceDetail: string[];
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get<Profile>("http://localhost:8000/api/profile")
+      .subscribe(data => {
+        this.performanceDetail = data.performanceDetail;
+        console.log(this.performanceDetail);
+      });
   }
 
 }
