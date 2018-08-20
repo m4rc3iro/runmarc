a1b9664e4e7add997c09d606f585a62a47b7be5a
marc <m4rc.3iro@gmail.com>
2018-08-20 20:38:49 +0200

    updating server.js config.

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('/dist/runmarc'));
 
 app.listen(process.env.PORT || 8080);
 
 //Path Location Strategy
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('/*', function(req, res) {
+  res.sendFile(path.join('/dist/runmarc/index.html'));
 });
 
 console.log('Console Listening');
