ac0f569941ba82bc8804a347f4f97c165d3c1dc9
m4rc3iro <m4rc.3iro@gmail.com>
2019-05-17 19:01:59 +0200

    Style adjustments to Timeline mobile.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
 	baseHref: '/',
+  api_admin_token: '',
   runmarc_api_base_url: 'http://134.209.239.31:3000'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
 	baseHref: '/',
+  api_admin_token: '',
   runmarc_api_base_url: 'http://localhost:3000'
 };
